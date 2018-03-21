7277c5b914ecf1faadabc64071f4c76b8e019733
Jesus Leon <jesusleon@outlook.com>
2018-03-21 10:41:14 -0300

    Se cambia foreground a blanco para background oscuros

---
+++ OrdVenta01/TipoDespachoToFrgndConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Drawing;
+
+namespace OrdVenta01
+{
+    public class TipoDespachoToFrgndConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int td = (int)value;
+            switch (td)
+            {
+                case 1:
+                    return "White";
+                case 2:
+                    return "Black";
+                case 3:
+                    return "White";
+                case 4:
+                    return "White";
+                case 5:
+                    return "White";
+                case 6:
+                    return "Black";
+                default:
+                    return "Black";
+
+
+
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
